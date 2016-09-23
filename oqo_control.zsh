1b11aa58e9cbe2266b27352be59baa35768e978a
indieterminacy <indieterminacy@nortonspeal.com>
2016-09-23 13:05:23 +0200

    Starting repo for zsh configs covering areas such as session managers and shell environments

---
+++ oqo_control.zsh
@@ -0,0 +1,22 @@
+#!/usr/bin/zsh
+
+#https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/gnu-utils
+zplug "peco/peco", \
+    as:command, \
+    from:gh-r
+# https://github.com/robbyrussell/oh-my-zsh/blob/master/plugins/common-aliases/common-aliases.plugin.zsh
+#https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/vi-mode
+#https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/vim-interaction
+#https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/systemadmin
+#
+#https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/debian
+#
+# Not sure what this does
+#https://github.com/robbyrussell/oh-my-zsh/blob/master/plugins/compleat/compleat.plugin.zsh
+
+####################
+# oq Tmux
+####################
+#https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/tmux
+#https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/tmuxinator
+#https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/tmux-cssh
